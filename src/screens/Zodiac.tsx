da3d244115d5f3829979763fcc892679cd7bf63a
DESKTOP-VIE32DC\Usuario <d2julian@gmail.com>
2024-08-22 20:50:18 +0200

    feat: added react-native-tab-view

--- src/screens/Zodiac.tsx
+++ src/screens/Zodiac.tsx
@@ -1,19 +1,44 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { useHoroscopeStore } from "@/store/useHoroscopeStore ";
 import { useShallow } from "zustand/react/shallow";
 import { ZodiacData } from "@/types/types";
 import { useMainHoroscope } from "@/selectors/mainZodiacSelector";
 
+import { TabView, SceneMap, TabBar } from "react-native-tab-view";
+import { WINDOW_WIDTH } from "@/UI/constants";
+
+const TodayRoute = () => <Text>Hoy</Text>;
+
+const GeneralRoute = () => <Text>General</Text>;
+
+const renderScene = SceneMap({
+  TodayRoute: TodayRoute,
+  GeneralRoute: GeneralRoute,
+});
+
 export default function Zodiac() {
   const [mainZodiac, userName] = useHoroscopeStore(useShallow((state) => [state.mainZodiac, state.userName]));
   const zodiac: ZodiacData | null = mainZodiac ? useMainHoroscope(mainZodiac) : null;
 
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: "TodayRoute", title: "Hoy" },
+    { key: "GeneralRoute", title: "General" },
+  ]);
+
+  const renderTabBar = (props) => (
+    <TabBar {...props} indicatorStyle={{ backgroundColor: "white" }} style={{ backgroundColor: "transparent" }} />
+  );
+
   return (
-    <View>
-      <Text>
-        {userName} {mainZodiac}
-      </Text>
-    </View>
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: WINDOW_WIDTH }}
+      renderTabBar={renderTabBar}
+      swipeEnabled={false}
+    />
   );
 }
